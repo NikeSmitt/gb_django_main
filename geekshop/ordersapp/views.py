488a609cc13c9fa11a721b5aaff114602ca6d4dc
gr <grim322@bk.ru>
2021-08-06 09:46:30 +0300

    tasks for hw 2-4 added

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
+from django.db.models.signals import pre_save, pre_delete
+from django.dispatch import receiver
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
@@ -18,55 +20,6 @@ class OrderList(ListView):
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
 
-
-# class OrderCreate(CreateView):
-#     model = Order
-#     fields = '__all__'
-#     context_object_name = 'object'
-#     success_url = reverse_lazy('ordersapp:orders_list')
-#
-#     def get_context_data(self, **kwargs):
-#         data = super(OrderCreate, self).get_context_data(**kwargs)
-#         OrderFormSet = inlineformset_factory(
-#             Order,
-#             OrderItem,
-#             form=OrderItemForm,
-#             extra=1
-#         )
-#
-#         if self.request.POST:
-#             formset = OrderFormSet(self.request.POST)
-#         else:
-#             basket_items = Basket.objects.filter(user=self.request.user)
-#             if len(basket_items):
-#                 OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=len(basket_items))
-#                 formset = OrderFormSet()
-#
-#                 for num, form in enumerate(formset.forms):
-#                     form.initial['product'] = basket_items[num].product
-#                     form.initial['quantity'] = basket_items[num].quantity
-#                 basket_items.delete()
-#             else:
-#                 formset = OrderFormSet()
-#
-#         data['orderitems'] = formset
-#         return data
-#
-#     def form_valid(self, form):
-#         context = self.get_context_data()
-#         orderitems = context['orderitems']
-#
-#         with transaction.atomic():
-#             form.instance.user = self.request.user
-#             self.object = form.save()
-#             if orderitems.is_valid():
-#                 orderitems.instance = self.object
-#                 orderitems.save()
-#
-#         if self.object.get_total_cost() == 0:
-#             self.object.delete()
-#
-#         return super(OrderCreate, self).form_valid(form)
 
 class OrderCreate(LoginRequiredMixin, CreateView):
     model = Order
@@ -88,6 +41,7 @@ class OrderCreate(LoginRequiredMixin, CreateView):
                 for num, form in enumerate(formset.forms):
                     form.initial['product'] = basket_items[num].product
                     form.initial['quantity'] = basket_items[num].quantity
+                    form.initial['price'] = basket_items[num].product.price
                 basket_items.delete()
             else:
                 formset = OrderFormSet()
@@ -127,6 +81,9 @@ class OrderUpdate(LoginRequiredMixin, UpdateView):
             formset = OrderFormSet(self.request.POST, instance=self.object)
         else:
             formset = OrderFormSet(instance=self.object)
+            for form in formset.forms:
+                if form.instance.pk:
+                    form.initial['price'] = form.instance.product.price
 
         data['orderitems'] = formset
 
@@ -170,3 +127,21 @@ def order_forming_complete(request, pk):
     order.save()
 
     return HttpResponseRedirect(reverse('ordersapp:orders_list'))
+
+
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def product_quantity_update_save(sender, update_fields, instance, **kwargs):
+    if update_fields is 'quantity' or 'product':
+        if instance.pk:
+            instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+        else:
+            instance.product.quantity -= instance.quantity
+        instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_delete(sender, instance, **kwargs):
+    instance.product.quantity += instance.quantity
+    instance.product.save()
